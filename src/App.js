168b7a45979c179004975628879bb7c69a928696
anhmmo <aun@metropolia.fi>
2020-04-02 14:49:46 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Loading from './components/Loading/Loading';
+import CardList from './components/CardList/CardList';
+class App extends Component {
+ /* constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+componentDidMount() {
+  window.onload = () => {
+    setTimeout(() => {
+        document.getElementById("load").style.display = 'none';
+        document.getElementById("pokemon").style.display = 'flex';
+    }, 2000);
+}
+}
+  render() {
+    return (
+      <div className="App">
+        <Loading />
+        <CardList />
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
