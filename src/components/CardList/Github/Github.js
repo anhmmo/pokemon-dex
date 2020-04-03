72b85d5eb09e82c2413bba62bc36e19f0e2828bb
anhmmo <aun@metropolia.fi>
2020-04-03 08:39:37 +0300

    create github label on top right window

---
+++ src/components/CardList/Github/Github.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Github.css';
+
+function Github() {
+    return (
+        <div>
+            <div className="github-link">
+                <a href="https://github.com/anhmmo/studentList" title="See Source Codes" target="_blank">See project <i class="fas fa-code" aria-hidden="true"></i> on Github</a>
+            </div>
+        </div>
+    )
+}
+
+export default Github
