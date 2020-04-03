8f93dfbe76b0d3430f8a44f87f0dc06efd15923d
anhmmo <aun@metropolia.fi>
2020-04-03 08:35:23 +0300

    import pikachu.png

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import './Loading.css';
+import pikachu from './pikachu.png';
+
 
 const Loading = () => {
     return (
         <div id="load">
-            <img src="https://dl.dropbox.com/s/o0m2gfnqtl8zd18/pikachu__1_-removebg-preview.png?raw=0"
-                className="imgbox" alt="pikachu" />
+            <img src={pikachu} className="imgbox" alt="pikachu" />
             <div id="loader">
                 <div className="pair pair-1">
-                <div className="dot dot-1" />
-                <div className="dot dot-2" />
+                    <span className="dot dot-1" />
+                    <span className="dot dot-2" />
                 </div>
                 <div className="pair pair-2">
-                <div className="dot dot-1" />
-                <div className="dot dot-2" />
+                    <span className="dot dot-1" />
+                    <span className="dot dot-2" />
                 </div>
                 <div className="loading-txt">Loading</div>
             </div>
