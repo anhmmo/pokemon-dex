8e2b02f10e13a6af61dedd6237d1060959e9a04d
anhmmo <aun@metropolia.fi>
2020-04-03 09:29:39 +0300

    add footer

---
+++ src/components/CardList/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>An open-source site by <a href="https://github.com/anhmmo">anhmmo</a>, redesign from <a href="https://codepen.io/FlorinPop17/full/gOYZxyE">Florin Pop</a> (Pokedex project) </p>
+            <p>All content is © Copyright of Nintendo, Game Freak, and The Pokémon Company. With help from Pokéapi.</p>
+        </div>
+    )
+}
+
+export default Footer
