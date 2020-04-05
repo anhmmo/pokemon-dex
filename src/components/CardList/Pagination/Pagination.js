6f4311fb337deab215c317ba2ab65fea0e199f14
anhmmo <aun@metropolia.fi>
2020-04-05 22:13:49 +0300

    create pagination pages

--- src/components/CardList/Pagination/Pagination.js
+++ src/components/CardList/Pagination/Pagination.js
@@ -2,8 +2,18 @@ import React from 'react'
 import './Pagination.css';
 function Pagination() {
     return (
-        <div>
-            
+        <div className="pagination">
+            <ul className="list-pages">
+                <li className="arrow-left active">&#129092;</li>
+                <li className="active">1</li>
+                <li className="pages">2</li>
+                <li className="pages">3</li>
+                <li className="pages">4</li>
+                <li className="pages">5</li>
+                <li>...</li>
+                <li className="pages">50</li>
+                <li className="arrow-right active">&#129094;</li>
+            </ul>
         </div>
     )
 }
