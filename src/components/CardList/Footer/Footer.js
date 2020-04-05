ad47982a0008c2bb6ec09009f8a2448e30948db5
anhmmo <aun@metropolia.fi>
2020-04-05 22:14:24 +0300

    add attribute open in new tab for a link

--- src/components/CardList/Footer/Footer.js
+++ src/components/CardList/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css'
 function Footer() {
     return (
         <div className="footer">
-            <p>An open-source site by <a href="https://github.com/anhmmo">anhmmo</a>, redesign from <a href="https://codepen.io/FlorinPop17/full/gOYZxyE">Florin Pop</a> (Pokedex project) </p>
+            <p>An open-source site by <a target="_blank" rel="noopener noreferrer" href="https://github.com/anhmmo">anhmmo</a>, redesign from <a target="_blank" rel="noopener noreferrer" href="https://codepen.io/FlorinPop17/full/gOYZxyE">Florin Pop</a> (Pokedex project) </p>
             <p>All content is © Copyright of Nintendo, Game Freak, and The Pokémon Company. With help from Pokéapi.</p>
         </div>
     )
