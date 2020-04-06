2a2d70db870bacc4c0b8eca8eed2291f9b36ff39
anhmmo <aun@metropolia.fi>
2020-04-06 12:57:44 +0300

    remove unneseccary function

--- src/components/CardList/Pagination/Pagination.js
+++ src/components/CardList/Pagination/Pagination.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Pagination.css';
-function Pagination({handleClick}) {
+function Pagination() {
     return (
         <div className="pagination">
             <ul className="list-pages">
                 <li className="arrow-left active">&#129092;</li>
                 <li className="active">1</li>
-                <li onClick={() => handleClick(20,40)} className="pages">2</li>
+                <li className="pages">2</li>
                 <li className="pages">3</li>
                 <li className="pages">4</li>
                 <li className="pages">5</li>
