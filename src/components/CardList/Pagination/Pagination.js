dd66b0432711c052af89d16114b64159e9bb20ad
anhmmo <aun@metropolia.fi>
2020-04-07 19:01:02 +0300

    create onClick function for each li element

--- src/components/CardList/Pagination/Pagination.js
+++ src/components/CardList/Pagination/Pagination.js
@@ -1,21 +1,34 @@
 import React from 'react'
 import './Pagination.css';
-function Pagination() {
+
+
+function Pagination({loadP}) {
+
+    Pagination.defaultProps = {
+         defaultBG: "pages",
+         activeBG: "active"
+    }
+
+    const {defaultBG, activeBG} = Pagination.defaultProps;
+    
     return (
         <div className="pagination">
             <ul className="list-pages">
+        
                 <li className="arrow-left active">&#129092;</li>
-                <li className="active">1</li>
-                <li className="pages">2</li>
-                <li className="pages">3</li>
-                <li className="pages">4</li>
-                <li className="pages">5</li>
+                <li onClick={() => loadP(0)} className={activeBG} >1</li>
+                <li onClick={() => loadP(1)} className={defaultBG}>2</li>
+                <li onClick={() => loadP(2)} className={defaultBG}>3</li>
+                <li onClick={() => loadP(3)} className={defaultBG}>4</li>
+                <li onClick={() => loadP(4)} className={defaultBG}>5</li>
                 <li>...</li>
-                <li className="pages">50</li>
+                <li onClick={() => loadP(39)} className={defaultBG}>41</li>
                 <li className="arrow-right active">&#129094;</li>
+
             </ul>
         </div>
     )
+    
 }
 
 export default Pagination
