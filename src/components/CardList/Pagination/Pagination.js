c1d643c1c668ac5eabba884e666ed019c197100c
anhmmo <aun@metropolia.fi>
2020-04-09 20:41:17 +0300

    remove default props

--- src/components/CardList/Pagination/Pagination.js
+++ src/components/CardList/Pagination/Pagination.js
@@ -1,35 +1,29 @@
 import React from "react";
 import "./Pagination.css";
 
-function Pagination({ loadP }) {
-  Pagination.defaultProps = {
-    defaultBG: "pages",
-    activeBG: "active",
-  };
-
-  const { defaultBG, activeBG } = Pagination.defaultProps;
-
+function Pagination(props) {
+  const { loadP, page, active, activeNum } = props;
   return (
     <div className="pagination">
       <ul className="list-pages">
         <li className="arrow-left active">&#129092;</li>
-        <li onClick={() => loadP(0)} className={activeBG}>
+        <li onClick={loadP(0)} className={activeNum === 0 ? active : page}>
           1
         </li>
-        <li onClick={() => loadP(1)} className={defaultBG}>
+        <li onClick={loadP(1)} className={activeNum === 1 ? active : page}>
           2
         </li>
-        <li onClick={() => loadP(2)} className={defaultBG}>
+        <li onClick={loadP(2)} className={activeNum === 2 ? active : page}>
           3
         </li>
-        <li onClick={() => loadP(3)} className={defaultBG}>
+        <li onClick={loadP(3)} className={activeNum === 3 ? active : page}>
           4
         </li>
-        <li onClick={() => loadP(4)} className={defaultBG}>
+        <li onClick={loadP(4)} className={activeNum === 4 ? active : page}>
           5
         </li>
         <li>...</li>
-        <li onClick={() => loadP(39)} className={defaultBG}>
+        <li onClick={loadP(39)} className={activeNum === 39 ? active : page}>
           41
         </li>
         <li className="arrow-right active">&#129094;</li>
