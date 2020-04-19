e882da3fe3834885bd051caaab4acb1beccca1db
anhmmo <aun@metropolia.fi>
2020-04-19 21:48:46 +0300

    change github link

--- src/components/CardList/Github/Github.js
+++ src/components/CardList/Github/Github.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import './Github.css';
+import React from "react";
+import "./Github.css";
 
 function Github() {
-    return (
-        <div>
-            <div className="github-link">
-                <a href="https://github.com/anhmmo/studentList" title="See Source Codes" target="_blank" rel="noopener noreferrer">See project <i className="fas fa-code" aria-hidden="true"></i> on Github</a>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="github-link">
+        <a
+          href="https://github.com/anhmmo/pokemon-dex"
+          title="See Source Codes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See project <i className="fas fa-code" aria-hidden="true"></i> on
+          Github
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Github
+export default Github;
