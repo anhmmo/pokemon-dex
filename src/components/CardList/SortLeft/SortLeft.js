d720bbd58776498fdd25d069b42d28aac1aaa013
anhmmo <aun@metropolia.fi>
2020-04-22 15:39:59 +0300

    add SortLeft to project

--- src/components/CardList/SortLeft/SortLeft.js
+++ src/components/CardList/SortLeft/SortLeft.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./SortLeft.css";
 
 function SortLeft({ onClickButton, checkOpenMenu, openMenu }) {
-  console.log(openMenu);
+  //console.log(openMenu);
   return (
     <div className="sorted-box">
       <div onClick={checkOpenMenu} className="label-box">
@@ -15,22 +15,22 @@ function SortLeft({ onClickButton, checkOpenMenu, openMenu }) {
       </div>
       {openMenu ? (
         <div className="show-box">
-          <div onClick={onClickButton} id="id" className="main-box">
+          <div onClick={onClickButton} name="1" className="main-box">
             <i className="fas fa-sort-numeric-down"></i> Show 20 Pokemon
           </div>
-          <div onClick={onClickButton} id="id2" className="main-box">
+          <div onClick={onClickButton} name="2" className="main-box">
             <i className="fas fa-sort-numeric-down-alt"></i>Show 50 Pokemon
           </div>
-          <div onClick={onClickButton} id="name" className="main-box">
+          <div onClick={onClickButton} name="3" className="main-box">
             <i className="fas fa-sort-alpha-down"></i>Show 100 Pokemon
           </div>
-          <div onClick={onClickButton} id="name2" className="main-box">
+          <div onClick={onClickButton} name="4" className="main-box">
             <i className="fas fa-sort-alpha-down-alt"></i>Show 150 Pokemon
           </div>
-          <div onClick={onClickButton} id="type" className="main-box">
+          <div onClick={onClickButton} name="5" className="main-box">
             <i className="fas fa-paw"></i>Show 200 Pokemon
           </div>
-          <div onClick={onClickButton} id="type2" className="main-box">
+          <div onClick={onClickButton} name="6" className="main-box">
             <i className="fab fa-typo3"></i>Show 300 Pokemon
           </div>
         </div>
