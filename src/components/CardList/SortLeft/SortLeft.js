bc760b014ec0524d8cbd62ced035d03437255ee4
anhmmo <aun@metropolia.fi>
2020-04-22 15:56:27 +0300

    change content in SortLeft

--- src/components/CardList/SortLeft/SortLeft.js
+++ src/components/CardList/SortLeft/SortLeft.js
@@ -6,7 +6,7 @@ function SortLeft({ onClickButton, checkOpenMenu, openMenu }) {
   return (
     <div className="sorted-box">
       <div onClick={checkOpenMenu} className="label-box">
-        Show Pokemon Number{" "}
+        Sort Pokemon In List
         {openMenu ? (
           <i className="fas fa-sort"></i>
         ) : (
@@ -16,22 +16,22 @@ function SortLeft({ onClickButton, checkOpenMenu, openMenu }) {
       {openMenu ? (
         <div className="show-box">
           <div onClick={onClickButton} name="1" className="main-box">
-            <i className="fas fa-sort-numeric-down"></i> Show 20 Pokemon
+            <i className="fas fa-sort-numeric-down"></i> Sort By Id (1 - 807)
           </div>
           <div onClick={onClickButton} name="2" className="main-box">
-            <i className="fas fa-sort-numeric-down-alt"></i>Show 50 Pokemon
+            <i className="fas fa-sort-numeric-down-alt"></i>Sort By Id (807 - 1)
           </div>
           <div onClick={onClickButton} name="3" className="main-box">
-            <i className="fas fa-sort-alpha-down"></i>Show 100 Pokemon
+            <i className="fas fa-sort-alpha-down"></i>Sort By Name (A - Z)
           </div>
           <div onClick={onClickButton} name="4" className="main-box">
-            <i className="fas fa-sort-alpha-down-alt"></i>Show 150 Pokemon
+            <i className="fas fa-sort-alpha-down-alt"></i>Sort By Name (Z - A)
           </div>
           <div onClick={onClickButton} name="5" className="main-box">
-            <i className="fas fa-paw"></i>Show 200 Pokemon
+            <i className="fas fa-paw"></i>Sort By Type (a - z)
           </div>
           <div onClick={onClickButton} name="6" className="main-box">
-            <i className="fab fa-typo3"></i>Show 300 Pokemon
+            <i className="fab fa-typo3"></i>Sort By Type (z - a)
           </div>
         </div>
       ) : (
